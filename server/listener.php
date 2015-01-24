0ae3618aaeb3f6bd2e5cbb05542557d1f465a8ca
forceapple <life147@live.com>
2015-01-23 17:20:17 -0800

    server

--- server/listener.php
+++ server/listener.php
@@ -1,12 +1,5 @@
 <?php
-include ("../model/user_CRUD_functions.php");
+include ("show.php");
 
-class adding{
-	function add_user(){
-		$data = $_POST["name"];
-		$data2 = $_POST["image"];
-		$db = new user_db();
-		$var = $db->add_user($data,$data2);
-	}
-}
+
 ?>
