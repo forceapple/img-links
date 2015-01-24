4ef6acdc3017d92568f5735214977798cf4e28cb
j33ff <j2fizgerald@gmail.com>
2015-01-23 16:12:25 -0800

    added frontend

--- view/main.php
+++ view/main.php
@@ -4,15 +4,22 @@
 <html>
 <head>
 <title>Lab</title>
+
+<link rel="stylesheet" type="text/css" href="../style/main.css">
 </head>
+
 
 <body>
 
-<form method="GET">
-<input name="input_one" type="text"></input>
-<input name="input_two" type="password"></input>
-<input  name="but "type="submit"></input>
+<div class="post-wrapper">
+	
+	<div class="images"><img src="http://images.boomsbeat.com/data/images/full/44019/puppy-wink_1-jpg.jpg"</img></div>
+
+	<div class="names">YO</div>
+</div>	
+
 </form>
+
 <?php
 	$var = new Numbers();
 	echo $var->give_username_pass($word);
