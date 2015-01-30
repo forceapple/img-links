16782ec65985ba7454aea82b0236b27bcfffb02d
Ilya Ulyanov <ilya.wt@gmail.com>
2015-01-30 14:52:45 -0800

    added empty functions

--- server/show.php
+++ server/show.php
@@ -32,13 +32,36 @@ class show {
 	}
 
 }
-//$user = new show();
-//$user->get_img();
-//echo $user->get_name();
-//Below are just for testing purpose;
-/*
-$var = new Numbers();
-echo $var->give_me_one();
-echo $var->give_me_two();
-*/
+
+	//returns a random noun (string)
+	public function get_random_noun(){
+
+	}
+
+	//returns a random verb (string)
+	public function get_random_verb(){
+
+	}
+
+	//returns a random name (string)
+	public function get_random_name(){
+
+	}
+
+	//passes the noun to DB to insert the noun
+	public function insert_noun($var){
+
+	}
+
+	//passes the verb to DB to insert the verb
+	public function insert_verb($var){
+
+	}
+
+	//passes the name to DB to insert the name
+	public function insert_name($var){
+
+	}
+
+
 ?>
