dff97f780fc632be5e9ed094087663e4a56466ae
forceapple <life147@live.com>
2015-01-30 15:50:26 -0800

    add

--- model/connect.php
+++ model/connect.php
@@ -1,5 +1,5 @@
 <?php
 
-$con = mysqli_connect("localhost", "root", "keeloskream", "mini_quiz_db") or die ("Failed to connect - ". mysqli_connect_error($con));
+$con = mysqli_connect("localhost", "root", "root", "quiz1") or die ("Failed to connect - ". mysqli_connect_error($con));
 
 ?>
