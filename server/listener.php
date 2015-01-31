e9eb5c458139e00a5629be2352927f3f4442bf59
Ilya Ulyanov <ilya.wt@gmail.com>
2015-01-30 16:36:01 -0800

    nifty changes

--- server/listener.php
+++ server/listener.php
@@ -7,7 +7,7 @@ $data = new show();
 if (isset($_POST['noun'])){
 	$user_noun = strip_tags($_POST['noun']);
 	if($data->insert_noun($user_noun)){
-		$_SESSION['status']   = "success";
+		$_SESSION['status']   = "Word $user_noun has been successfuly inserted";
 	}else{
 		$_SESSION['status']   = "fail";
 	}
@@ -16,7 +16,7 @@ if (isset($_POST['noun'])){
 if (isset($_POST['verb'])){
 	$user_verb = strip_tags($_POST['verb']);
 	if($data->insert_verb($user_verb)){
-		$_SESSION['status']   = "success";
+		$_SESSION['status']   = "Word $user_verb has been successfuly inserted";
 	}else{
 		$_SESSION['status']   = "fail";
 	}
@@ -25,7 +25,7 @@ if (isset($_POST['verb'])){
 if (isset($_POST['name'])){
 	$user_name = strip_tags($_POST['name']);
 	if($data->insert_name($user_name)){
-		$_SESSION['status']   = "success";
+		$_SESSION['status']   = "Word $user_name has been successfuly inserted";
 	}else{
 		$_SESSION['status']   = "fail";
 	}
