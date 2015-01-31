ceeeaba368c48134d4e0f4399bd84fa7110e0b9a
Ilya Ulyanov <ilya.wt@gmail.com>
2015-01-30 16:07:54 -0800

    t

--- model/user_CRUD_functions.php
+++ model/user_CRUD_functions.php
@@ -14,13 +14,13 @@
 		}
 		function insert_noun($noun) {
 			global $con;
-			$query = "INSERT INTO nouns (`name`) VALUES ('".$noun."')";
+			$query = "INSERT INTO nouns (`noun`) VALUES ('".$noun."')";
 			$result = mysqli_query($con, $query);
 		
 		}
 		function insert_verb($verb) {
 			global $con;
-			$query = "INSERT INTO verbs ( `name`) VALUES ('".$verb."')";
+			$query = "INSERT INTO verbs ( `verb`) VALUES ('".$verb."')";
 			$result = mysqli_query($con, $query);
 		
 		}
